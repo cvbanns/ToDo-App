03b1e86863854375761d452ff46e5a4d10c3c20d
Caleb Aregbesola <37102400+cvbanns@users.noreply.github.com>
2022-11-28 14:32:09 +0100

    Initial commit
    AppData, DataAccessLayer, Controllers

---
+++ Controllers/MyTaskController.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ToDoApp.DataAccessLayer;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    public class MyTaskController : Controller
+    {
+        TaskDataAccessLayer _taskDataAccessLayer = new TaskDataAccessLayer();
+        // GET: MyTask
+        public ActionResult Index(string searchBy, string searchValue)
+        {
+            
+            var taskList = _taskDataAccessLayer.GetAllTheTasks();
+            if (taskList.Count == 0)
+            {
+                TempData["InfoMessage"] = "No tasks to Diplay Currently.";
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(searchValue))
+                {
+                    TempData["InfoMessage"] = "Provide search value.";
+                    return View(taskList);
+                }
+                else
+                {
+                    if(searchBy.ToLower() == "task")
+                    {
+                        var searchByTask = taskList.Where(p => p.Task.ToLower().Contains(searchValue.ToLower()));
+                        return View(searchByTask);
+                    }
+                    else if(searchBy.ToLower() == "due")
+                    {
+                        var searchByDueDate = taskList.Where(p => p.Task.ToLower().Contains(searchValue.ToLower()));
+                        return View(searchByDueDate);
+                    }
+                }
+            }
+            
+            return View(taskList);
+        }
+
+        // GET: MyTask/Details/5
+        public ActionResult Details(int id)
+        {
+            var myTask = _taskDataAccessLayer.GetTasksByID(id).FirstOrDefault();
+            try
+            {
+                if (myTask == null)
+                {
+                    TempData["InfoMessage"] = "Task does not exist";
+                    return RedirectToAction("Index");
+                }
+                return View(myTask);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: MyTask/Create
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        // POST: MyTask/Create
+        [HttpPost]
+        public ActionResult Create(MyTask myTask)
+        {
+            bool IsInserted = false;
+            
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    IsInserted = _taskDataAccessLayer.InsertTask(myTask);
+
+                    if (IsInserted)
+                    {
+                        TempData["SuccessMessage"] = "Task added Successfully";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Unable to save Task/Task exists already";
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: MyTask/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var myTask = _taskDataAccessLayer.GetTasksByID(id).FirstOrDefault();
+
+            if(myTask == null)
+            {
+                TempData["InfoMessage"] = "Task does not exist";
+                return RedirectToAction("Index");
+            }
+            return View(myTask);
+        }
+
+        // POST: MyTask/Edit/5
+        [HttpPost,ActionName("Edit")]
+        public ActionResult UpdateTask(MyTask myTask)
+        {
+            
+            try
+            {
+                // TODO: Add update logic here
+                if (ModelState.IsValid)
+                {
+                    bool IsUpdated = _taskDataAccessLayer.UpdateTask(myTask);
+                    if (IsUpdated)
+                    {
+                        TempData["SuccessMessage"] = "Task Updated Successfully";
+                    }
+                    else
+                    {
+                        TempData["ErrorMessage"] = "Unable to save Task/Task exists already";
+
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // GET: MyTask/Delete/5
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var myTask = _taskDataAccessLayer.GetTasksByID(id).FirstOrDefault();
+
+                if (myTask == null)
+                {
+                    TempData["ErrorMessage"] = "Task does not exist";
+                    return RedirectToAction("Index");
+                }
+                return View(myTask);
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        // POST: MyTask/Delete/5
+        [HttpPost,ActionName("Delete")]
+        public ActionResult DeleteConfirmation(int id)
+        {
+            string result = _taskDataAccessLayer.DeleteTask(id);
+            
+            try
+            {
+                // TODO: Add delete logic here
+                if (result.Contains("Deleted"))
+                {
+                    TempData["SuccessMessage"] = result;
+                }
+                else
+                {
+                    TempData["ErrorMessage"] = result;
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["ErrorMessage"] = ex.Message;
+                return View();
+            }
+        }
+
+        //GET : MyTask/DeleteAllTask
+        public ActionResult DeleteAllTask()
+        {
+            return View();
+        }
+
+        //POST:
+        [HttpPost]
+        public ActionResult DeleteAllTask(MyTask mystask)
+        {
+            bool IsDeletedAll = false;
+            try
+            {
+                IsDeletedAll = _taskDataAccessLayer.DeleteAllTasks();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return View();
+            }
+
+            
+        }
+    }
+}
---
+++ DataAccessLayer/TaskDataAccessLayer.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using ToDoApp.Models;
+using System.Data;
+
+namespace ToDoApp.DataAccessLayer
+{
+    public class TaskDataAccessLayer
+    {
+        string conString = ConfigurationManager.ConnectionStrings["todoConnectionstring"].ToString();
+
+        //Get All The Tasks
+        public List<MyTask> GetAllTheTasks()
+        {
+            List<MyTask> taskList = new List<MyTask>();
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_GetAllTheTasks";
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                DataTable dtTasks = new DataTable();
+
+                connection.Open();
+                sqlDataAdapter.Fill(dtTasks);
+                connection.Close();
+
+                foreach (DataRow dataRow in dtTasks.Rows)
+                {
+                    taskList.Add(new MyTask
+                    {
+                        TaskID = Convert.ToInt32(dataRow["TaskID"]),
+                        Task = dataRow["Task"].ToString(),
+                        Details = dataRow["Details"].ToString(),
+                        Due = dataRow["Due"].ToString(),
+                        Completed = dataRow["Completed"].ToString()
+                    });
+                }
+            }
+
+            return taskList;
+        }
+
+        //Insert Task
+        public bool InsertTask(MyTask myTask)
+        {
+            int id = 0;
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("sp_InsertTasks", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@Task", myTask.Task);
+                command.Parameters.AddWithValue("@Details", myTask.Details);
+                command.Parameters.AddWithValue("@Due", myTask.Due);
+                command.Parameters.AddWithValue("@Completed", myTask.Completed);
+
+                connection.Open();
+                id = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return id > 0;
+        }
+
+        //Get Tasks by Task ID
+        public List<MyTask> GetTasksByID(int TaskID)
+        {
+            List<MyTask> taskList = new List<MyTask>();
+
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = connection.CreateCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "sp_GetTaskByID";
+                command.Parameters.AddWithValue("@TaskID", TaskID);
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                DataTable dtTasks = new DataTable();
+
+                connection.Open();
+                sqlDataAdapter.Fill(dtTasks);
+                connection.Close();
+
+                foreach (DataRow dataRow in dtTasks.Rows)
+                {
+                    taskList.Add(new MyTask
+                    {
+                        TaskID = Convert.ToInt32(dataRow["TaskID"]),
+                        Task = dataRow["Task"].ToString(),
+                        Details = dataRow["Details"].ToString(),
+                        Due = dataRow["Due"].ToString(),
+                        Completed = dataRow["Completed"].ToString()
+                    });
+                }
+            }
+
+            return taskList;
+        }
+
+        //InsUpdateert Task
+        public bool UpdateTask(MyTask myTask)
+        {
+            int i = 0;
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("sp_UpdateTasks", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@TaskID", myTask.TaskID);
+                command.Parameters.AddWithValue("@Task", myTask.Task);
+                command.Parameters.AddWithValue("@Details", myTask.Details);
+                command.Parameters.AddWithValue("@Due", myTask.Due);
+                command.Parameters.AddWithValue("@Completed", myTask.Completed);
+
+                connection.Open();
+                i = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return i > 0;
+        }
+
+        //Delete One Task
+        public string DeleteTask(int taskid)
+        {
+            string result = "";
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("sp_DeleteTask", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.AddWithValue("@taskid", taskid);
+                command.Parameters.Add("@OutputMessage", SqlDbType.NVarChar, 50).Direction = ParameterDirection.Output;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                result = command.Parameters["@OutputMessage"].Value.ToString();
+                connection.Close();
+            }
+            return result;
+        }
+
+        public bool DeleteAllTasks()
+        {
+            int result = 0;
+            using (SqlConnection connection = new SqlConnection(conString))
+            {
+                SqlCommand command = new SqlCommand("sp_DeleteAllTask", connection);
+                command.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                result = command.ExecuteNonQuery();
+                connection.Close();
+            }
+            return result != 0;
+        }
+    }
+}
