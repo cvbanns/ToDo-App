b20191534d2f346a89ccaad8d4b5f212e17ddf72
Caleb Aregbesola <37102400+cvbanns@users.noreply.github.com>
2022-11-28 14:33:47 +0100

    Models

---
+++ Models/MyTask.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp.Models
+{
+    public class MyTask
+    {
+        [Key]
+        public int TaskID { get; set; }
+        [Required]
+        [DisplayName("Task")]
+        public string Task { get; set; }
+        [Required]
+        public string Details { get; set; }
+        [Required]
+        public string Due { get; set; }
+        [Required]
+        public string Completed { get; set; }   
+    }
+}
